96e9526f0f43673d37d00809e9b6aa208b232fc8
wusel <wusel+src@uu.org>
2023-10-13 02:15:04 +0200

    Update for 4830

--- site.mk
+++ site.mk
@@ -11,7 +11,7 @@ ebtables-filter-multicast
 ebtables-filter-ra-dhcp
 ebtables-limit-arp
 mesh-batman-adv-15
-mesh-vpn-wireguard
+mesh-vpn-tunneldigger
 radv-filterd
 radvd
 respondd
@@ -36,7 +36,6 @@ GLUON_FEATURES_standard := $(GLUON_FEATURES_standard:\n= )
 #       chosen feature flags
 define GLUON_SITE_PACKAGES :=
 ffac-ssid-changer
-ffac-wg-registration
 iwinfo
 respondd-module-airtime
 endef
