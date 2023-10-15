fbb59df8925b20bc9d0545d90b89036e07dc54db
wusel <wusel+src@uu.org>
2023-10-15 06:21:07 +0200

    Go back to 2023.1+x

--- release.mk
+++ release.mk
@@ -1,2 +1,3 @@
 GLUON_GIT_URL := https://github.com/freifunk-gluon/gluon.git
-GLUON_GIT_REF := 7feebe96a6b93d567bd342ffd01abcb3022383c5 # v2023.1.1 #1b6ddac3c608961399c4877198dd552c12c3f07b # master
+#GLUON_GIT_REF := 7feebe96a6b93d567bd342ffd01abcb3022383c5 # v2023.1.1
+GLUON_GIT_REF := 1b6ddac3c608961399c4877198dd552c12c3f07b # master
